8719623ba122f29d054088b4ea1ba0c4e184c5b3
Marco Perretti <marco.perretti@ergonresearch.it>
2026-02-02 12:18:54 +0100

    feat(1): demo workflow jenkins

--- SimpleJenkins/src/main/java/com/tutorial/simplejenkins/SimpleJenkinsApplication.java
+++ SimpleJenkins/src/main/java/com/tutorial/simplejenkins/SimpleJenkinsApplication.java
@@ -3,7 +3,8 @@ package com.tutorial.simplejenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.tutorial.simplejenkins", "tutorial.controller"})
+@SpringBootApplication(
+    scanBasePackages = {"com.tutorial.simplejenkins", "com.tutorial.simplejenkins.controller"})
 public class SimpleJenkinsApplication {
 
   public static void main(String[] args) {
--- SimpleJenkins/src/main/java/com/tutorial/simplejenkins/controller/CheckUpController.java
+++ SimpleJenkins/src/main/java/com/tutorial/simplejenkins/controller/CheckUpController.java
@@ -1,4 +1,4 @@
-package tutorial.controller;
+package com.tutorial.simplejenkins.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +10,12 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/health")
 public class CheckUpController {
-    @GetMapping
-    public ResponseEntity<Map<String, String>> checkHealth() {
-        return ResponseEntity.ok(Map.of(
-                "status", "UP",
-                "message", "Server is up and running!",
-                "timestamp", java.time.LocalDateTime.now().toString()
-        ));
-    }
+  @GetMapping
+  public ResponseEntity<Map<String, String>> checkHealth() {
+    return ResponseEntity.ok(
+        Map.of(
+            "status", "UP",
+            "message", "Server is up and running!",
+            "timestamp", java.time.LocalDateTime.now().toString()));
+  }
 }
